ffd37d1ddf520164ba7c7ad06e5c136e01783bdd
Robin Truong <robintrg007@gmail.com>
2020-03-21 11:26:13 -0700

    Removed print statements

--- Assignments/Assignment 1/Minion Tracker/src/Main.java
+++ Assignments/Assignment 1/Minion Tracker/src/Main.java
@@ -3,10 +3,6 @@ import java.util.List;
 
 public class Main {
     public static void main(String[] args){
-        System.out.println("hello world");
-        System.out.println("hello world");
-        System.out.println("hello world");
-
         List<Minions> listOfMinions = new ArrayList<>();
         Menu menu = new Menu(listOfMinions);
     }
