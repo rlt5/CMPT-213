443b31598922e0c1109a6b49e9b2c6d2a44c6d97
Robin Truong <robintrg007@gmail.com>
2020-03-21 03:04:12 -0700

    Add print statements

--- Assignments/Assignment 1/Minion Tracker/src/Main.java
+++ Assignments/Assignment 1/Minion Tracker/src/Main.java
@@ -3,6 +3,11 @@ import java.util.List;
 
 public class Main {
     public static void main(String[] args){
+        System.out.println("hello world");
+        System.out.println("hello world");
+        System.out.println("hello world");
+        System.out.println("hello world");
+        System.out.println("hello world");
         List<Minions> listOfMinions = new ArrayList<>();
         Menu menu = new Menu(listOfMinions);
     }
