a41c1fe9e790f410c42455ccd2256fbb3bba2c63
Robin Truong <robintrg007@gmail.com>
2020-03-21 02:25:03 -0700

    changed filename

---
+++ Assignments/Assignment 1/Minion Tracker2/src/Main.java
@@ -0,0 +1,9 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class Main {
+    public static void main(String[] args){
+        List<Minions> listOfMinions = new ArrayList<>();
+        Menu menu = new Menu(listOfMinions);
+    }
+}
